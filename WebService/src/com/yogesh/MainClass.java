c118d1ade8e27324e206d7e273c54ec2852adf23
yogesh.kumar <m18nikhilkumar@gmail.com>
2016-03-03 16:17:03 +0530

    Useless

--- WebService/src/com/yogesh/MainClass.java
+++ WebService/src/com/yogesh/MainClass.java
@@ -6,7 +6,6 @@ public class MainClass {
 	
 	public static void main(String[] arg){
 		
-		System.out.println("This is main method");
 		Endpoint.publish("http://localhost:8080/WebService/Calculate",new Calculator());
 		System.out.println("Service Publised");
 	}
